fcbed410735c8667da1b7be155de103b470582ea
Proxitrone <proxitrone@gmail.com>
2021-01-20 12:01:17 +0800

    TF1 version of horovod training
    Changed gpu information extraction and allocation to workers to follow tensorflow 1 api

--- machine-learning-tutorials/distributed_classification_with_Horovod/distributed_train.py
+++ machine-learning-tutorials/distributed_classification_with_Horovod/distributed_train.py
@@ -17,13 +17,11 @@ import horovod.keras as hvd
 # Initialize Horovod
 hvd.init()
 
-# Pin GPU to be used to process local rank (one GPU per process)
-gpus = tf.config.experimental.list_physical_devices('GPU')
-print(gpus)
-for gpu in gpus:
-    tf.config.experimental.set_memory_growth(gpu, True)
-if gpus:
-    tf.config.experimental.set_visible_devices(gpus[hvd.local_rank()], 'GPU')
+# Tensorflow 1: Pin GPU to be used to process local rank (one GPU per process)
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+config.gpu_options.visible_device_list = str(hvd.local_rank())
+K.set_session(tf.Session(config=config))
 
 
 batch_size = 128
