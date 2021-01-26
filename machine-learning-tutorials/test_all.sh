16e44e32901f8c8204d528bfb640f90f2fdc3532
SimonChuang <simon.s.chuang@gmail.com>
2021-01-27 00:32:00 +0800

    Add data_augmentation.ipynb

--- machine-learning-tutorials/test_all.sh
+++ machine-learning-tutorials/test_all.sh
@@ -2,6 +2,9 @@
 bash distributed_classification_with_Horovod/test.sh
 rm distributed_classification_with_Horovod/test.sh
 
+# Test data_augmentation.ipynb
+jupyter nbconvert --to notebook --inplace --execute data_augmentation.ipynb
+
 # Remove itself
 rm -- "$0"
 
